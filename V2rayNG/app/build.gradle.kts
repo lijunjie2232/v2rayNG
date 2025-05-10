49a15a3f51f90f86e4eb5791ba3d1cef4991f741
lijunjie2232 <2533584225@qq.com>
2025-05-10 20:18:54 +0900

    test

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -9,11 +9,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "cn.xuexi.android"
         minSdk = 21
         targetSdk = 35
-        versionCode = 652
-        versionName = "1.10.2"
+        versionCode = 888888
+        versionName = "8.88.88"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
--- V2rayNG/settings.gradle.kts
+++ V2rayNG/settings.gradle.kts
@@ -20,5 +20,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "v2rayNG"
+rootProject.name = "学习强国"
 include(":app")
